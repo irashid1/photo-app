6ade428e30d35ac196a1c7ed4bd2f67c9f17413c
Imtiaz <imtiaz.rashid.89@gmail.com>
2022-06-11 16:23:08 -0400

    saved faces of thailand into firebase, mapped the info and succesfully displayed it on to page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
 import './styles/sass/App.scss';
+import { firebase, instagramDatabase } from './firebase';
 
-import GetImages from './components/GetImages';
+// Add the push function to our list of Firebase module imports:
+import { ref, onValue } from 'firebase/database';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [dataBase, setDataBase] = useState([]);
+
+  useEffect(() => {
+    const allData = ref(instagramDatabase, "data/");
+    onValue(allData, (snapshot) => {
+      const data = snapshot.val();
+      const newArr = [];
+      if (data !== null) {
+        for (let i in data) {
+          newArr.push(
+            {
+              post: data[i]
+            }
+          );
+        }
+      }
+      setDataBase(newArr);
+    })
+  }, [])
+
+
+  console.log(dataBase);
+
   return (
     <div className="App">
-      
+
       <header className="App-header">
         <h1>Photo App</h1>
       </header>
 
       <main>
-        <GetImages />
+        {dataBase.map((info) => {
+          console.log(info.post.children.data)
+          return (
+            <div key={info.post.id}>
+              {info.post.children.data.map((img) => {
+                return (
+                  <div>
+                    <img src={img.media_url} alt="ig-img"/>
+                  </div>
+                )
+              })}
+              <p>
+                {info.post.caption}
+              </p>
+            </div>
+          )
+        })}
       </main>
 
       <footer>
-       
+
       </footer>
 
     </div>
@@ -28,7 +72,13 @@ export default App;
 // pseudo code 
 
 // create components
-// get data from the instagram api 
+// get data from firebase
+// map each individual element on to a state
+  // sort data based on
+    // children > data > every child > id & media_url (also their id as keys)
+    // caption
+    // id as key
+    // results based on #facesoffood
 // output info on page 
 // sort information based on user selection
 // have a grid layout for the images
---
+++ src/firebase.js
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from 'firebase/database';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ8qGrtVHkq2p57GXawwK63v7t473WiiI",
+    authDomain: "photo-app-40b0e.firebaseapp.com",
+    projectId: "photo-app-40b0e",
+    storageBucket: "photo-app-40b0e.appspot.com",
+    messagingSenderId: "541485290871",
+    appId: "1:541485290871:web:59564375ffdac475b17d72"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+export const instagramDatabase = getDatabase(firebase);
+
+// const database = firebase.database();
+
+// const ref = database.ref('data')
+// ref.on('value', gotData, errData);
+
+// function gotData(data) {
+//     console.log(data.val());
+// }
+
+
+// function errData(err) {
+//     console.log('Error!');
+//     console.log(err);
+// }
+
+export default firebase;
